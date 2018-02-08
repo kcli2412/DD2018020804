64ab2e02478951f6ca41a3f91536f5b08679f061
kcli2412 <kcli2412@gmail.com>
2018-02-08 16:56:00 +0800

    BroadcastReceiver

--- app/src/main/java/com/example/student/dd2018020804/MyReceiver.java
+++ app/src/main/java/com/example/student/dd2018020804/MyReceiver.java
@@ -11,6 +11,6 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Log.d("REC", "onReceive: ");
+        Log.d("REC", "DD2018020804 onReceive: ");
     }
 }
